db946cdba001b8f2e80a96e17d665841b3b563fb
bimalesh12345 <bimalesh54321@gmail.com>
2019-06-20 16:47:13 +0530

    Updates

---
+++ precache-manifest.ead0c576384bf4c57aaefe1b4ca5a6b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acec385123e75f5ab25c0fa569a126ca",
+    "url": "/myproject/index.html"
+  },
+  {
+    "revision": "d284749cc9f15f6c1500",
+    "url": "/myproject/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "941b7c42f7913ac24e1d",
+    "url": "/myproject/static/css/main.4159d1f8.chunk.css"
+  },
+  {
+    "revision": "d284749cc9f15f6c1500",
+    "url": "/myproject/static/js/2.eb546de8.chunk.js"
+  },
+  {
+    "revision": "941b7c42f7913ac24e1d",
+    "url": "/myproject/static/js/main.3915d5bd.chunk.js"
+  },
+  {
+    "revision": "9f8ccf630f8fb1fa091c",
+    "url": "/myproject/static/js/runtime~main.1e800c5e.js"
+  }
+]);
